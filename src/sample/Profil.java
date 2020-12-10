507c1582362701fe282d59d49c10099fab017cbf
torjmen <torjmenspo@outlook.fr>
2020-12-10 22:44:45 +0100

    bouttIN, bouttOUT...

--- src/sample/Profil.java
+++ src/sample/Profil.java
@@ -1,17 +1,30 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.MouseEvent;
 
 public class Profil {
 
-    double xOffset,yOffset;
+    double xOffset, yOffset;
     public Label x;
+
     public void sauve(MouseEvent mouseEvent) {
     }
 
     public void init(ContextMenuEvent contextMenuEvent) {
     }
 
+    public void bouttIn(MouseEvent event) {
+        x.setOpacity(0.3);
+    }
+
+    public void bouttOut(MouseEvent event) {
+        x.setOpacity(1);
+    }
+
+    public void bouttonExt(MouseEvent event) {
+        Platform.exit();
+    }
 }
